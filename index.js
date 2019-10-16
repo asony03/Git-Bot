e18ff06fa3c303c5e515db4258eb0985ba2f9a67
Harsh Agrawal <hagrawa2@ncsu.edu>
2019-10-16 00:58:23 -0400

    Working webhooks for public gitbot repository

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/payload', (req, res) => {
+  // console.log(req);
+  console.log(req.body);
+});
+
 app.listen(port, () => console.log('Gitbot running on port 3000'));
+
