b1c0d570c2c3c770e66e2ee03c556dbde2338cf3
Sharique <sharique101311cse@gmail.com>
2019-10-22 03:14:05 -0400

    test scenario 2 added

--- test/IntegrationTests.js
+++ test/IntegrationTests.js
@@ -13,10 +13,20 @@ var userName = "khanshariquem";
 var password = "juit@101311";
 var repository = "GitBot-Test";
 var issueName = "GitBot-Test-Issue" + Date.now();
-// browser.get('http://en.wikipedia.org/wiki/Wiki');
-// browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then(function(links){
-//     console.log('Found', links.length, 'Wiki links.' )
-// });
+var issueComment = "GitBot-Test-Comment"+ Date.now();
+
+var slackLoginLink = "https://slack.com/signin";
+var slackworkspace = "freefolk-at-ncsu";
+var slackUserName = "mkhan8@ncsu.edu";
+var slackPassword = "password@123";
+var slackChannel = "se-project";
+
+
+var chromeCapabilities = webdriver.Capabilities.chrome();
+var chromeOptions = {
+    'args': ['--no-sandbox']
+};
+chromeCapabilities.set('chromeOptions', chromeOptions);
 
 (async function test() {
     try {
@@ -24,7 +34,7 @@ var issueName = "GitBot-Test-Issue" + Date.now();
             this.timeout(50000);
 
             beforeEach (async () => {
-                browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
+                browser = new webdriver.Builder().usingServer().withCapabilities(chromeCapabilities).build();
                 await browser.get(githubLoginLink);
                 //await browser.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS );
                 await browser.findElement(webdriver.By.id("login_field")).sendKeys(userName);
@@ -49,6 +59,34 @@ var issueName = "GitBot-Test-Issue" + Date.now();
                 await browser.navigate().refresh();
                 var labels = await browser.findElement(webdriver.By.xpath("//details[@id='labels-select-menu']/parent::div/div")).getText();
                 expect(labels).to.not.include('None yet');
+            });
+
+            it ('find the rpository, Comment on an issue and validate slack integration', async () => {
+                //issueName = 'GitBot-Test-Issue1571727951325';
+                //comment on the issue created in first test
+                await browser.findElement(webdriver.By.xpath("(//div[@role='search' and @aria-label='Repositories' ]/input)[1]")).sendKeys(repository);
+                await browser.sleep(1000);  
+                await browser.findElement(webdriver.By.xpath("(//div[@class='js-repos-container']//li[contains(.,'"+repository+"')]//a)[1]")).click();
+                await browser.sleep(1000); 
+                await browser.findElement(webdriver.By.xpath("(//a//span[contains(., 'Issues')]//parent::a)")).click(); 
+                await browser.findElement(webdriver.By.xpath("//div[@aria-label='Issues']//a[contains(., '"+issueName+"')]")).click();
+                await browser.findElement(webdriver.By.id("new_comment_field")).sendKeys(issueComment);
+                await browser.findElement(webdriver.By.xpath("//button[@type='submit' and contains(.,'Comment')]")).click(); 
+                
+                //slack integration
+                await browser.get(slackLoginLink);
+                await browser.findElement(webdriver.By.id("domain")).sendKeys(slackworkspace);
+                await browser.findElement(webdriver.By.id("submit_team_domain")).click();
+                await browser.sleep(2000);
+                await browser.findElement(webdriver.By.id("email")).sendKeys(slackUserName);
+                await browser.sleep(2000);
+                await browser.findElement(webdriver.By.id("password")).sendKeys(slackPassword);
+                await browser.sleep(2000);
+                await browser.findElement(webdriver.By.id("signin_btn")).click();
+                await browser.findElement(webdriver.By.xpath("(//div[contains(@class, 'channel_sidebar')])[1]//a[contains(., '"+slackChannel+"')]")).click();
+                await browser.sleep(2000);
+                var elemeents = await browser.findElements(webdriver.By.xpath("//a[contains(., 'Test-comment')]"))
+                expect(elemeents.length).to.be.gt(0);
             });
         });
     } catch (ex) {
